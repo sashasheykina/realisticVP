a194b339c66ef933593bde02dea8c77fe27c56f4
sashasheykina <alexandrasheykina@gmail.com>
2022-01-15 23:05:00 +0100

    add REPORT

--- boxplot.py
+++ boxplot.py
@@ -4,14 +4,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set_theme(style="ticks", palette="pastel")
-data_url = 'experiments_kmeans_cmeans.csv'
+data_model = 'experiments_kmeans_cmeans.csv'
 # read data from url as pandas dataframe
-gapminder = pd.read_csv(data_url)
+df_model = pd.read_csv(data_model)
 # subset the gapminder data frame for rows with year values 1952 and 2007
-df1 = gapminder[gapminder['model'].isin(["kmeans","cmeans"])]
+my_mcc = df_model[df_model['model'].isin(["kmeans","cmeans"])]
 sns.boxplot(y='mcc', x='balancing',
-                 data=df1,
-                 palette="colorblind", #palette=["m", "g"]
+                 data=my_mcc,
+                 palette=["m", "g"], #palette="colorblind"
+                 hue='model')
+sns.despine(offset=10, trim=True)
+plt.show()
+my_recall = df_model[df_model['model'].isin(["kmeans","cmeans"])]
+sns.boxplot(y='recall', x='balancing',
+                 data=my_recall,
+                 palette=["m", "g"],
+                 hue='model')
+sns.despine(offset=10, trim=True)
+plt.show()
+my_acc = df_model[df_model['model'].isin(["kmeans","cmeans"])]
+sns.boxplot(y='accuracy', x='balancing',
+                 data=my_acc,
+                 palette=["m", "g"],
                  hue='model')
 sns.despine(offset=10, trim=True)
 plt.show()
